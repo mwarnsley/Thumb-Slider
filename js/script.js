5aa3a215e86b77e77fae88d86ab1ae2648b2f17c
Warnsley <Marcus.Warnsley@navient.com>
2016-04-11 17:44:55 -0400

    Setting up the intial javascript for the slides and looping through them

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-ECHO is on.
+$(function(){
+	// Declare vars
+	var totalWidth = 0;
+	var positions = new Array();
+	
+	$('#slides .slide').each(function(i){
+		// Get slider widths
+		positions[i] = totalWidth;
+		totalWidth += $(this).width();
+		
+		// Check widths
+		if(!$(this).width()){
+			alert('Please add a width to your images');
+			return false;
+		}
+	});
+});
