56a34023fb6853ecffbe1170028a11046122c06d
Warnsley <Marcus.Warnsley@navient.com>
2016-04-11 17:46:59 -0400

    Setting the width and setting the menu item click handler

--- js/script.js
+++ js/script.js
@@ -14,4 +14,25 @@ $(function(){
 			return false;
 		}
 	});
+	// Set width
+	$('#slides').width(totalWidth);
+	
+	// Menu item click handler
+	$('#menu ul li a').click(function(e, keepScroll){
+		// Remove active class and add inactive
+		$('li.product').removeClass('active').addClass('inactive');
+		// Add active class to parent
+		$(this).parent().addClass('active');
+		
+		var pos  = $(this).parent().prevAll('.product').length;
+		
+		$('#slides').stop().animate({marginLeft:-positions[pos]+'px'}, 450);
+		
+		// Prevent default
+		e.preventDefault();
+		
+		// Stop autoscroll
+		if(!autoScroll) clearInterval(itvl);
+	});
+	
 });
